67d6705fc20ed36ae060faa15511357cab0bf5ab
Jordan Queiroga <jordanpqueiroga@gmail.com>
2020-04-10 15:54:51 -0300

    Commit inicial com eslint configurado

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Oi</Text>
+    </View>
+  );
+}
+
+
+export default App;
